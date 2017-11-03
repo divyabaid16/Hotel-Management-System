01cbdf6ed6f376ac0f5fc7da24658da1bbfdd855
Divya Baid <32747809+divyabaid16@users.noreply.github.com>
2017-11-03 19:38:42 +0530

    Basic Calculator

--- main.cpp
+++ main.cpp
@@ -4,6 +4,34 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    cout<<"Choose an option"<<endl;
+    cout<<"1. Addition \n2. Subtraction \n3. Multiplication \n4. Division"<<endl;
+    int a;
+    int b;
+    int operation;
+    int value;
+    cin>>operation;
+    cout<<"Enter first no.";
+    cin>>a;
+    cout<<"Enter second no.";
+    cin>>b;
+
+    if(operation==1){
+        value=a+b;
+    }
+    else if(operation==2){
+        value=a-b;
+    }
+    else if(operation==3){
+        value=a*b;
+    }
+    else if(operation==4){
+        value=a/b;
+    }
+
+    else{
+        cout<<"invalid choice";
+    }
+    cout<<"The result is "<<value <<endl;
     return 0;
 }
