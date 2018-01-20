e6677331531a2177cc9cf960851008783302a626
Divya Baid <32747809+divyabaid16@users.noreply.github.com>
2018-01-20 21:50:46 +0530

    Final Update

--- Room.h
+++ Room.h
@@ -10,13 +10,19 @@ class Room
         Room();
         int roomNo;
         int roomType;
+        int rate;
+        int no;
         bool checkAvaility();
         int chooseRoom();
         int getRoomRate();
         void displayItems(int type);
-        int bookRoom();
+        void bookRoom();
         void check_room_no();
         int getRoomNo();
+        void setRoomNo(int a);
+        int getNo();
+        int getType();
+        int getRate();
     protected:
 
     private:
