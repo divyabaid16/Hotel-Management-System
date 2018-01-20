76042f1aab1b04e9a9b18c216f661f5bd6a615da
Divya Baid <32747809+divyabaid16@users.noreply.github.com>
2018-01-20 21:46:06 +0530

    Final Update Guest
    This class contains the function to add, delete, search and view all the records.

--- Guest.cpp
+++ Guest.cpp
@@ -12,95 +12,76 @@ Guest::Guest()
 }
 
 
+Guest::Guest(int a,int b,int c){
+    room_no=a;
+    roomType=b;
+    roomRate=c;
+}
+
+
 
     void Guest::inputData(){
-        char nm[30];
-        //int id=0;
-        id=id1;
-        id1++;
-        long int ph;
+       fstream file;
+       string na;
+       char nm[30];
+       char ph[14];
         char ct[30];
-        //int room_no;
-        int ci[3];
-        int co[3];
-        //char roomType[30];
-        //int roomRate;
-        int f;
+
+    file.open("record.txt",ios::out|ios::app);
+    if(!file)
+    {
+      cout<<"Error in creating file.."<<endl;
+
+    }
+    cout<<"\nFile created successfully."<<endl;
 
 
 
-        Room r;
-   // id=0;
-     room_no=r.getRoomNo();
-     //roomRate=0;
-
-   /*     Room r;
-        f=r.chooseRoom();
-        roomType=f;
-
-
-        roomRate=r.getRoomRate();*/
-       //cout <<"Enter Room type:\n";
-       //cin >>roomType;
-    cout <<"\nChecking Availabity.......\n";
-
+    while(1){
     cout <<"Enter your Check In date(DD-MM-YYYY):\n";
-
-
     Date d1;
-    cout <<"Enter Day:";
-    cin >>ci[0];
-    d1.d=ci[0];
-    cout <<"Enter Month:";
-    cin >>ci[1];
-    d1.m=ci[1];
-    cout <<"Enter Year:";
-    cin >>ci[2];
-    d1.y=ci[2];
-
-
-   // Date d1;
-    //int* in=d1.getDate();
-    //setCD(in);
-
-    //cin >> ci;
     setCheckInDate(d1.d,d1.m,d1.y);
 
-    //Date d1;
-//    for(int j=0;j<3;j++)ci[j]=d1.getDate();
-    //cout <<roomType;
     cout <<"Enter your Check Out date:\n";
     Date d2;
-     cout <<"Enter Day:";
-    cin >>co[0];
-    d1.d=co[0];
-    cout <<"Enter Month:";
-    cin >>co[1];
-    d1.m=co[1];
-    cout <<"Enter Year:";
-    cin >>co[2];
-    d1.y=co[2];
     setCheckOutDate(d2.d,d2.m,d2.y);
 
+    int diff=d1.getDifference(d1,d2);
+    difference=diff;
 
-/*    bool a=r.checkAvaility();
-
-    if(a==true){
-        cout <<"Room is available\n";
+    if(diff>0){
+        setPrice(diff);
+         break;
     }
-    */
-
-     cout <<"Enter Name:\n";
+    else if(diff==0)
+        cout<<"\nCheck In and Check Out Date can't be same\n";
+    else cout<<"\nCheck Out can't be before than Check In\n";
+    }
+    cout <<"Enter Name:\n";
     cin >> nm;
     setName(nm);
 
-    cout <<"Enter Phone No.:\n";
+    cout <<"Enter your 10 digit mobile no. :\n";
     cin >> ph;
     setPhno(ph);
 
     cout <<"Enter City:\n";
     cin >>  ct;
     setCity(ct);
+    int i;
+
+    //Entering the record in the file
+    file<<name<<" "<<ph_no<<" "<<city<<" "<<room_no<<" "; for(i=0;i<3;i++)
+             {file<<checkIn[i];
+              if(i<2)
+                file<<"-";
+             }file<<" ";for(i=0;i<3;i++)
+             {file<<checkOut[i];
+              if(i<2)
+                file<<"-";
+             }file<<" "<<roomType<<" "<<roomRate<<" "<<price<<endl;
+
+    file.close();
 }
 
 
@@ -108,9 +89,7 @@ Guest::Guest()
 
    void Guest::setCheckInDate(int d,int m,int y){
     checkIn[0] = d;
-//    checkIn[1]="-";
     checkIn[1]= m;
-  //  checkIn[3]="-";
     checkIn[2]=y;
 
     }
@@ -121,9 +100,7 @@ Guest::Guest()
 
     void Guest::setCheckOutDate(int d,int m,int y){
     checkOut[0] = d;
-//    checkIn[1]="-";
     checkOut[1]= m;
-  //  checkIn[3]="-";
     checkOut[2]=y;
 
 
@@ -141,18 +118,54 @@ Guest::Guest()
     return name;
     }
 
-    void Guest::setPhno(long int ph){
-    ph_no=ph;
+
+
+    void Guest::setPhno(char* ph){
+
+        for(int i=0; i < 10; ++i){
+    ph_no[i] = ph[i];
     }
-    long int Guest::getPhno(){
+
+    }
+    char* Guest::getPhno(){
     return ph_no;
     }
 
+
+     void Guest::setNewPhone(char* ph){
+        for(int i=0; i < 10; ++i){
+    phhhh[i] = ph[i];
+    }
+
+    }
+
+    int Guest::getRoomNo(){
+        return room_no;
+    }
     void Guest::setCity(char* ct){
     for(int i=0; i < 10; ++i){
     city[i] = ct[i];
     }
     }
+
+    void Guest::setPrice(int pr){
+        price=pr*roomRate;
+        }
+
+    bool Guest::check(char *a){
+        int j=0;
+        char k[6]="123";
+        for(int i=0;i<3;i++){
+            if(k[i]==a[i])
+                j++;
+        }
+        if(j==3)
+    return true;
+    else
+        return false;
+    }
+
+
     char* Guest::getCity(){
     return city;
     }
@@ -160,174 +173,189 @@ Guest::Guest()
     //displaying the food items available along with their price
     //storing the total sum and adding it to the total bill
     }
-    void Guest::payBills(){
-    //getting all the payable amounts
-    cout << "Summary:";
-    cout<<"\n Cusromer Details";
-            cout<<"\n ****************";
-            cout<<"\n\n Name: "<<name;
-            cout<<"\n\n ID: "<<id;
-            cout<<"\n\n Phone No: "<<ph_no;
-            cout<<"\n\n City: "<<city;
-            cout<<"\n\n Room No: "<<room_no;
 
-            cout<<"\n\n Check In: "<< checkIn;
-            cout<<"\n\n Check Out: "<<checkOut;
-            cout<<"\n\n Room Type: "<<roomType;
-            cout<<"\n\n Room Rate: "<<roomRate;
 
-    }
-    string Guest::printBill(){
-    cout<<"Total bill: \n";
-
-    cout<<"Others:";
-    }
     string Guest::submitFeedback(){
    // A form will be provided to be filled
     }
 
-    void Guest::summary(){
-       Guest obj1;
-    int eno;
-    char ch = 'n';
 
-    ifstream file1("record.dat", ios::binary); // this file should already exist
-
-    cout << "Enter the phone no : " <<endl;
-    cin >> eno;
-
-    while(file1.read((char *)&obj1, sizeof(obj1)))
-    {
-        //!file1.eof()
-        //;
-
-        if(obj1.getPhno()==eno)
-        {
-            obj1.putdata();
-            ch = 'y';
-            //break;
-        }
-    }
-
-    if(ch =='n')
-        cout << "Record Not Found !!" << endl;
-    file1.close();
-    }
 
     void Guest::putdata()
               {
-         /*         cout<<"\n Cusromer Details";
-            cout<<"\n ****************";
-            cout<<"\t\t"<<name;
-            cout<<"\t\t"<<id;
-            cout<<"\t\t"<<ph_no;
-            cout<<"\t\t"<<city;
-            cout<<"\t\t"<<room_no;
-            cout<<"\t\t"<< checkIn;
-            cout<<"\t\t"<<checkOut;
-            cout<<"\t\t"<<roomType;
-            cout<<"\t\t"<<roomRate;
-            */
+
+                  fstream file;
+        file.open("record.txt",ios::in);
+
+
+
+    if(!file){
+        cout<<"No record is present";
+
+    }
+    int q=0;
+
+  //while(file.good()){
+   // while (getline(file, line)){
+ while(file>>name>>ph_no>>city>>room_no>>checkIn[0]>>checkIn[1]>>checkIn[2]>>checkOut[0]>>checkOut[1]>>checkOut[2]>>roomType>>roomRate>>price){
+
+    file>>name;
+    file>>ph_no;
+    file>>city;
+    file>>room_no;
+    for(int i=0;i<3;i++)
+             {file>>checkIn[i];
+             }for(int i=0;i<3;i++)
+             {file>>checkOut[i];
+             }
+    file>>roomType;
+    file>>roomRate;
+    file>>price;
             int i;
+            int q=roomType;
+            string typeOfRoom;
+            switch(q){
+             case 1:{typeOfRoom="Single room";
+            }
+            break;
+    case 2:{typeOfRoom="Double room";
+            }
+            break;
+    case 3:{typeOfRoom="Family room";
+            }
+            break;
+    case 4:{typeOfRoom="King sized room";
+            }
+            break;}
+
+            //Printing all the records
             cout<<"\n Cusromer Details";
             cout<<"\n ****************";
             cout<<"\t Name: "<<name;
-            cout<<"\t ID: "<<id;
             cout<<"\t Phone No: "<<ph_no;
             cout<<"\t City: "<<city;
             cout<<"\t Room No: "<<room_no;
             cout<<"\t Check In(DD-MM-YYYY): ";
              for(i=0;i<3;i++)
              {cout<<checkIn[i];
-              if(i<2)
-                cout<<"-";
+             }
+
+            cout<<"\t Check Out(DD-MM-YYYY): ";
+             for(i=0;i<3;i++)
+             {cout<<checkOut[i];
+             }
+            cout<<"\t Room Type: "<<typeOfRoom;
+            cout<<"\t Room Rate: "<<roomRate;
+            cout<<"\t Total Price: "<<price;
+            cout<<"\n\n";
+
+
+   }
+                file.close();
+
+              }
+
+void Guest::searchRecord(){
+       fstream file;
+        file.open("record.txt",ios::in);
+    if(!file){
+        cout<<"Error in opening file..";
+
+    }
+    char nm[30];
+    setName(nm);
+    char ph[14];
+    char ct[30];
+    // while (!file.eof()){
+     int i=0,j=0;
+    // cout<<ph_no;
+        cout<<"Enter the phone no";
+        cin>>ph;
+        setNewPhone(ph);
+
+
+        //Searching a record through phone no.
+    while(file>>name>>ph_no>>city>>room_no>>checkIn[0]>>checkIn[1]>>checkIn[2]>>checkOut[0]>>checkOut[1]>>checkOut[2]>>roomType>>roomRate>>price){
+
+        int r=0;
+        for(int i=0;i<10;i++){
+
+            if(phhhh[i]==ph_no[i])
+                r++;
+        }
+
+
+        if(r==10){
+                int i;
+                j++;
+                cout<<"\n Cusromer Details";
+            cout<<"\n ****************";
+            cout<<"\t Name: "<<name;
+            cout<<"\t Phone No: "<<ph_no;
+            cout<<"\t City: "<<city;
+            cout<<"\t Room No: "<<room_no;
+            cout<<"\t Check In(DD-MM-YYYY): ";
+             for(i=0;i<3;i++)
+             {cout<<checkIn[i];
              }
 
             cout<<"\t Check Out(DD-MM-YYYY): ";
              for(i=0;i<3;i++)
              {cout<<checkIn[i];
-              if(i<2)
-                cout<<"-";
              }
             cout<<"\t Room Type: "<<roomType;
             cout<<"\t Room Rate: "<<roomRate;
             cout<<"\n\n";
+            break;
+        }
+     }
+      if(j==0){
+            cout<<"\n\nNo matching record found!\n\n";
+        }
+      file.close();
+
 }
-    void Guest::writeIntoFile(){
-        Guest gst;
-        ofstream file;
-        file.open("record.dat",ios::binary|ios::app);
 
+//Deleting the record through room no
+void Guest::deleteRecord(){
+    int a;
+    cout<<"Enter room no.";
+    cin>>a;
+     string line1;
+    ifstream fp1;
+fp1.open("record.txt");
+ofstream file;
+file.open("temp_room.txt");
+int flag=0;
+while(fp1>>name>>ph_no>>city>>room_no>>checkIn[0]>>checkIn[1]>>checkIn[2]>>checkOut[0]>>checkOut[1]>>checkOut[2]>>roomType>>roomRate>>price){
+       if(room_no==a)
+            flag=1;
 
-    //open file in write mode
+    if(room_no!=a){
 
-    if(!file)
-    {
-      cout<<"Error in creating file.."<<endl;
-      //return 0;
-    }
-    cout<<"\nFile created successfully."<<endl;
-    gst.inputData();
-    file.write((char*)&gst,sizeof(gst));
-    file.close();   //close the file
-    cout<<"\nFile saved and closed succesfully."<<endl;
+            int i;
 
-    }
+        file<<name<<" "<<ph_no<<" "<<city<<" "<<room_no<<" "; for(i=0;i<3;i++)
+             {file<<checkIn[i];
+             }file<<" ";for(i=0;i<3;i++)
+             {file<<checkIn[i];
+             }file<<" "<<roomType<<" "<<roomRate<<endl;
 
-    void Guest::getCompleteRecord(){
-        Guest gst;
-
-     ifstream fin;
-     fin.open("record.dat",ios::binary);
-
-    while(fin.read((char*)&gst,sizeof(Guest)))
-    {
-        gst.putdata();
-    }
-    fin.close();
 
     }
 
-    void Guest::deleteRecord(){
-        ifstream fd;
-        ofstream ft;
-        fd.open("record.dat",ios::binary);
-        ft.open("temp.dat",ios::binary|ios::app);
-        int flagd=0;
-        char conf='y';
-        Guest ed;
-        cout<<"Enter the phone no.:";
-        int enodel; cin>>enodel;
-        while(fd.read((char*)&ed,sizeof(ed)))//!fd.eof())
-        {
-            //fd.read((char*)&ed,sizeof(ed));
-            if(ed.getPhno()==enodel)
-            {
-                flagd=1;
-                cout<<"record preparing for delete";
-                ed.putdata();
-                cout<<"do you want do delete this record(y/n):";
-                cin>>conf;
-                if(conf=='n')
-                ft.write((char*)&ed,sizeof(ed));
-            }
-             ft.write((char*)&ed,sizeof(ed));
-        }
-        if(flagd==0)
-        cout<<"Record not found ";
-        remove("record.dat");
-        rename("temp.dat","record.dat");
-        cout<<"process complete";
-        fd.close(); ft.close();
-        }
+}
+if(flag==0)
+    cout<<"\nRecord Not Found\n";
+else
+    cout<<"\nRecord Deleted\n";
+fp1.close();
+file.close();
+remove("record.txt");
+rename("temp_room.txt","record.txt");
+
+}
 
 
 
-   /*     void Guest::setCD(int *in){
-            checkIn[0] = &in[0];
-//    checkIn[1]="-";
-    checkIn[1]= &in[1];
-  //  checkIn[3]="-";
-    checkIn[2]=&in[2];
-        }
-*/
+
